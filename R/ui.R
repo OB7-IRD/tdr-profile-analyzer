e43621b80c8d6ed8bd05c7241746062f5d3b1e54
ChloeTellier <chloe.tellier@ird.fr>
2024-10-23 15:39:08 +0200

    Organisation du code (separation ui et server)

---
+++ R/ui.R
@@ -0,0 +1,70 @@
+ui <- dashboardPage(
+  dashboardHeader(title = "TDR PROFILE ANALYZER - v0.1",
+                  titleWidth = 500),
+  dashboardSidebar(disable = TRUE),
+  dashboardBody(
+    fluidRow(
+      box(
+        title = "1. Sélection du fichier .csv (NKE WiSens TD1000 v2)", 
+        width = 12,
+        status = "primary", solidHeader = TRUE,
+        
+        column(12,
+               # Input : le fichier .csv d'entrée
+               column(8,
+                      fileInput("file1", label = NULL,
+                                buttonLabel = "Parcourir",
+                                placeholder = "Pas de fichier sélectionné",
+                                multiple = FALSE,
+                                accept = c("text/csv",
+                                           "text/comma-separated-values,text/plain",
+                                           ".csv"))),
+               
+               # Numéro de série, dates de marée
+               column(4,
+                      textOutput("tdrmodel"),
+                      textOutput("numerodeserie"))),
+        
+        # Partie de l'UI a afficher uniquement si un fichier
+        # d'entrée a bien été sélectionné
+        uiOutput("fichier_entree")
+      )
+    ),
+    fluidRow(
+      box(
+        title = "2. Profil TDR et données clés", 
+        width = 12,
+        status = "primary", solidHeader = TRUE,
+        
+        column(6,
+               h5("Sur le profile suivant, cliquez pour placer dans l'ordre :"),
+               h5("- (A) Le début du déploiement"),
+               h5("- (B) Le début de la pêche"),
+               h5("- (C) La fin de la pêche (début de la remontée ou capture d'un individu)"),
+               h5("- (D) La fin du déploiement (la fin de la remontée)"),
+               h5("Veillez à bien cliquer sur les points dans l'ordre demandé."),
+               h5("Les points que vous avez sélectionnés s'affichent en rouge sur le graphique, et leurs coordonnées dans le tableau ci-dessous.")),
+        
+        column(6,
+               h5("Si vous vous êtes trompés, cliquez sur le bouton Recommencer."),
+               actionButton("clear_points", "Recommencer"),
+               h5("Une fois que vous avez sélectionné les 4 points demandés, cliquez sur le bouton Calculer pour obtenir les métriques qui nous intéressent."),
+               actionButton("calculate", "Calculer")),
+        
+        # Partie de l'UI a afficher uniquement si un fichier
+        # d'entrée a bien été sélectionné
+        uiOutput("graphe_et_tables")
+      )
+    ),
+    fluidRow(
+      box(
+        title = "3. Téléchargements", 
+        width = 12,
+        status = "primary", solidHeader = TRUE,
+        
+        downloadButton("download_plot", "Télécharger figure en .png")
+        # downloadButton("downloadData", "Download table")
+      )
+    )
+  )
+)
